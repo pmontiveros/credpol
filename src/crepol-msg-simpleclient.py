b341a116d4c51c51c9f0e2e6696a3a1ad1ff5d46
Leonardo Ezequiel Hernandez <leohernandez@deloitte.com>
2024-10-10 12:47:18 -0300

    Debug mode to Simpleclient

--- src/crepol-msg-simpleclient.py
+++ src/crepol-msg-simpleclient.py
@@ -104,4 +104,4 @@ def service_status():
     })
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=CLIENT_SERVICE_PORT)
+    app.run(host='0.0.0.0', port=CLIENT_SERVICE_PORT, debug=True)
